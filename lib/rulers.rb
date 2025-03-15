7e6c51c665029847c91595d17b88db331af86b6b
utsubo256 <greeeentea256@gmail.com>
2025-03-15 14:00:36 +0900

    First Controller

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require 'rulers/routing'
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
-      [200, { 'content-type' => 'text/html' }, ['Hello from Ruby on Rulers!']]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, { 'content-type' => 'text/html' }, []]
+      end
+
+      if env['PATH_INFO'] == '/'
+        return [200, { 'content-type' => 'text/html' }, ['root path!!!']]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, { 'content-type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
